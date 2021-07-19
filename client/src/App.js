13ca9dd876fd4b493ed6a29e0cf250f0f6a4202b
Alejandro Gonzalvez <agonzalvez1@gmail.com>
2021-07-19 13:30:04 -0400

    Styling updated. Signup and login pages created

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from './components/pages/Home'
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+import Login from './components/pages/Login';
+import Signup from './components/pages/Signup';
+import { BounceLoader, Barloader, BeatLoader } from "react-spinners";
+import "react-datepicker/dist/react-datepicker.css";
 import './App.css';
 
 
@@ -13,6 +17,12 @@ export default function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home}/>
+        </Switch>
+        <Switch>
+          <Route path="/login" exact component={Login}/>
+        </Switch>
+        <Switch>
+          <Route path="/sign-up" exact component={Signup}/>
         </Switch>
       </Router>
     </>
