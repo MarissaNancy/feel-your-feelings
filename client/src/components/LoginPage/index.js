35a4927af272b863c5b6f0acf09c889c129c047a
Alejandro Gonzalvez <alejandrogonzalvez@Alejandros-MacBook-Pro.local>
2021-07-26 19:23:46 -0400

    Commit test

--- client/src/components/LoginPage/index.js
+++ client/src/components/LoginPage/index.js
@@ -1,57 +1,85 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "../LoginPage/style.css";
 import { Button } from '../Button/index';
-
-
-
-
+import API from '../../utils/API';
+import { useHistory } from "react-router-dom";
 function LoginPage() {
+    const [login, setLogin] = useState({})
+    const [signup, setSignup] = useState({})
+    let history = useHistory();
+useEffect(() => {
+    console.log(login)
+    console.log(signup)
+},[login, signup])
+function handleLoginInputChange(event){
+    const { name, value } = event.target;
+    setLogin({...login, [name]: value})
+}
+function handleSignupInputChange(event){
+    const { name, value } = event.target;
+    setSignup({...signup, [name]: value})
+}
+function handleLoginFormSubmit(event){
+     event.preventDefault();
+    API.login(login)
+    .then(res => {
+        console.log(res)
+        history.push("/dashboard");
+    })
+}
+function handleSignupFormSubmit(event){
+    event.preventDefault();
+    API.signup(signup)
+    .then(res => {
+        console.log(res)
+        history.push("/dashboard");
+    })
+}
     return (
         <form className="login-form">
             <label className="login-label">
                 <h1>Email:</h1>
-                <input className="email-field" type="text" name="email" />
+                <input className="email-field" type="text" name="email" onChange={handleLoginInputChange} />
                 <h1>Password:</h1>
-                <input className="password-field" type="text" name="password" />
-                <Button className="login-btn" buttonStyle="login--btn" buttonSize="btn--large">Login</Button>
+                <input className="password-field" type="text" name="password" onChange={handleLoginInputChange} />
+                <Button className="login-btn" buttonStyle="login--btn" buttonSize="btn--large" onClick={handleLoginFormSubmit}>Login</Button>
                 {/* <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
                             Sign Up
                         </Link> */}
             </label>
-
             <image className="logo"></image>
-
             <label className="signup-label">
                 <h1>Name:</h1>
                 <input
                     className="name-field"
                     type="text"
                     name="name"
-                    placeholder="username" />
+                    placeholder="username" 
+                    onChange={handleSignupInputChange} />
                 <h1>Email:</h1>
                 <input
                     className="email-field"
                     type="text"
                     name="email"
-                    placeholder="email" />
+                    placeholder="email" 
+                    onChange={handleSignupInputChange} />
                 <h1>Password:</h1>
                 <input
                     className="password-field"
                     type="text"
                     name="password"
-                    placeholder="password" />
+                    placeholder="password" 
+                    onChange={handleSignupInputChange} />
                 <h1>Confirm Password:</h1>
                 <input
                     className="password-field"
                     type="text"
                     name="password2"
-                    placeholder="confirm password" />
-                <Button className="signup-btn" type="submit" buttonStyle="signup--btn" buttonSize="btn--large">Sign Up</Button>
-
+                    placeholder="confirm password" 
+                    onChange={handleSignupInputChange} />
+                <Button className="signup-btn" type="submit" buttonStyle="signup--btn" buttonSize="btn--large" onClick={handleSignupFormSubmit}>Sign Up</Button>
             </label>
-
         </form>
     )
 }
-
 export default LoginPage
